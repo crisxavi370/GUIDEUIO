7a41b93a29b1c04546e7a546b97bab459ec17a2f
crisxavi370 <viscainocristian@gmail.com>
2022-02-02 18:12:10 -0500

    Commit

--- app/src/main/java/com/example/adoptame/casosUso/NewsUseCase.kt
+++ app/src/main/java/com/example/adoptame/casosUso/NewsUseCase.kt
@@ -6,27 +6,41 @@ class NewsUseCase {
 
     private val newsList = listOf<News>(
         News(
-            "Michael L Hicks ",
-            "The Microsoft-Activision acquisition targets Google and Meta more than Sony",
-            "This deal could help sell more Xbox Series Xs in the short term, but Microsoft has much bigger VR/AR fish to fry. Microsoft claims its recent Activision Blizzard acquisition is about more than just gaming: it will provide building blocks for the metaverse…",
-            "https://www.androidcentral.com/sites/androidcentral.com/files/styles/large/public/article_images/2022/01/microsoft-logo-2017-ignite2.jpeg"
+            "Cumbre del Panecillo, Gral Melchor Aymerich, Quito EC170150, Ecuador",
+            "VIRGEN DE EL PANECILLO",
+            "El Panecillo es una elevación natural de 3.000 metros sobre el nivel del mar, fue bautizada con este nombre por su parecido con un pequeño pan; está enclavada en el corazón mismo de la ciudad de Quito. Es uno de los sitios más visitados de la ciudad. Por su ubicación se ha convertido en el más importante mirador natural de la ciudad, desde el que se puede apreciar la disposición urbana de la capital, desde su centro histórico y hacia los extremos norte y sur.\n" +
+                    "\n" +
+                    "El lugar recibió su nombre de los conquistadores españoles, pues antes era llamado por los aborígenes como “Shungoloma” que en quichua significa “loma del corazón”. En la época pre incaica, en este sitio existió un templo de adoración al sol, llamado “Yavirac”, el cual fue destruido por Rumiñahui mientras resistía con sus tropas al avance español.",
+            "https://imagenes.laestrella.com.pa/files/image_990_491/uploads/2019/09/16/5d7f49e0deb2c.jpeg"
         ),
         News(
-            "Nicola Davis Science correspondent",
-            "Can they fix it? UK project to explore ability and desire to repair tech",
-            "Exclusive: Experts aim to find out whether there are hotspots around country where electronic waste is avoidedFrom fancy toys to smartphones, when technology breaks, it often seems simplest to ditch it for a new model.But now experts are hoping to challenge t…",
-            "https://i.guim.co.uk/img/media/4af13fbee58e29226a7b128b201fe72c7fe5c2ae/0_254_5760_3456/master/5760.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=ff100d9185a80cbe5d3be5729293ce24"
+            "Fulgencio Araujo, y, Quito 170527",
+            "Teleférico de Quito",
+            "El teleférico es uno de lugares turísticos de Quito que no debes dejar de visitar. Es uno de los más altos de Sudamérica: su recorrido de 18 minutos aproximadamente comienza a 3.000 metros y culmina a los 4.100. Desde esas alturas tienes impresionantes vistas panorámicas de la ciudad y sus alrededores.\n" +
+                    "En los alrededores del teleférico hay uno de los sitios para acampar en Quito, además se pueden hacer cabalgatas o vuelos en parapente. Todos los servicios se contratan en la base del teleférico.\n",
+            "https://teleferico.com.ec/wp-content/uploads/2019/04/imagen_inicio3.jpg"
         ),
         News(
-            "Editor David",
-            "Blender 3.0 Released With More New Features and Improvements",
-            "Long-time Slashdot reader Qbertino writes: The Free Open Source 3D production software Blender has been released in version 3.0 (official showreel) with more new features, improvements and performance optimizations as well as further improved workflows. \n\nIn …",
-            "https://a.fsdn.com/sd/topics/graphics_64.png"
-        ), News(
-            "Aglaia Berlutti",
-            "La tercera temporada de ‘Servant’ revela finalmente los grandes misterios de la serie",
-            "Durante dos temporadas, la serie Servant de Apple+ creada por M. Night Shyamalan ha sido una combinación de elementos dispares. Los primeros capítulos, estrenados en el 2019, analizaron la idea sobre la pérdida, el dolor y el miedo desde un espacio misterioso…",
-            "https://i0.wp.com/hipertextual.com/wp-content/uploads/2019/10/hipertextual-primer-teaser-trailer-servant-serie-m-night-shyamalan-apple-tv-2019757980-scaled.jpg?fit=2560%2C1600&ssl=1"
+            "Av. Manuel Córdova Galarza SN, Quito",
+            "Mitad del Mundo",
+            "El país lleva su nombre por la curiosidad de estar atravesado de este a oeste por la línea ecuatorial. Para celebrar esa singularidad se edificó uno de 10 lugares turísticos del Ecuador más visitados. En el sitio hay un monumento y otras construcciones. Con respecto al primero hay dos datos curiosos: primero, que el actual es una réplica del original que medía 10 metros y que ahora se emplaza en la ciudad Calacali; segundo, que en realidad la línea equinoccial se ubica a 240 metros hacia el sur de donde se encuentra el monolito.\n" +
+                    "En las cercanías hay varios atractivos como la Plaza Cultural con banquitos tipo anfiteatros en donde se realizan conciertos los fines de semana. Pero si buscas lugares románticos en Quito, la ciudad tiene una capilla donde se efectúan enlaces matrimoniales. Hay ofertas gastronómicas como la Plaza del Cacao. Ahí se puede conocer cómo es la producción, la preparación y el consumo del fruto.\n",
+            "https://cloudfront-us-east-1.images.arcpublishing.com/eluniverso/IJL6QOFW7ZCWBHWUAE37RUNKAI.jpg"
+        ),
+        News(
+            "García Moreno, Quito 170401",
+            "Palacio de Carondelet",
+            "En todo el centro de la ciudad se halla uno de los lugares históricos de Quito más conocidos: el edificio que funciona como sede del Gobierno del Ecuador. Desde 2007 se abrió para recibir visitas guiadas y dar a conocer sus diferentes áreas. Entre ellas puedes recorrer el Salón Amarillo, el Salón de Presidentes, el Salón de Banquetes, el Oratorio y el Salón de Gabinete.\n" +
+                    "En la edificación hay exposiciones permanentes con los obsequios, tanto nacionales como internacionales, entregados al primer mandatario. Destacan las centenarias sillas que fueron talladas por el artista italiano Juan Bautista Minghetti, el mismo que realizó los bocetos del monumento a la Independencia, situado en el corazón de la Plaza Grande.\n",
+            "https://live.staticflickr.com/3062/2873544803_acddc90af8_b.jpg"
+        ),
+        News(
+            "Av. 6 de Diciembre 345, Quito 170136",
+            "Casa de la Cultura Ecuatoriana",
+            "Uno de los intelectuales más destacados de Ecuador en el siglo XX fue Benjamín Carrión Mora. Este personaje promovió el desarrollo de la cultura y la libertad de pensamiento en Ecuador, y creó la Casa de la Cultura Ecuatoriana (CCE) en agosto de 1944.\n" +
+                    "\n" +
+                    "La cultura de la ciudad, los museos, las publicaciones están presentes en todo el país en 24 sucursales de la Casa de la Cultura, pero la sede nacional está en Quito y puedes incluirla en tu itinerario para una visita bastante cultural.",
+            "https://www.ecuadorhop.com/wp-content/uploads/2020/01/casa-de-la-cultura-de-quito-lugares-turisticos-de-quito-web.jpg"
         )
     )
 
